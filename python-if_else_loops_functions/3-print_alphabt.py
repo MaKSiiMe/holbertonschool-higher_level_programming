59f3e78cfb648a6242b5c288c65d1d7a76ac2463
MaKSiiMe <maxime.truel@gmail.com>
2025-01-16 16:30:05 +0100

    change format print

--- python-if_else_loops_functions/3-print_alphabt.py
+++ python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-for l in range(97, 123):
-    if chr(l) != 'q' and chr(l) != 'e':
-        print(chr(l), end="")
+for i in range(97, 123):
+    if chr(i) != 'q' and chr(i) != 'e':
+        print("{:c}".format(i), end="")
--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for n in range(100):
-    if n < 99:
-        print(f"{n:02d}, ", end="")
+for i in range(100):
+    if i < 99:
+        print("{:02d}, ".format(i), end="")
     else:
-        print(f"{n:02d}")
+        print("{:02d}".format(i))
