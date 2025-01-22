36aa9bf0de21842f9a6ce07c677dd4ad3d8c6a2c
MaKSiiMe <maxime.truel@gmail.com>
2025-01-22 15:52:26 +0100

    update 0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 def safe_print_list(my_list=[], x=0):
     i = 0
-    for i in range(x):
+    for j in range(x):
         try:
-            print("{:d}".format(my_list[i]), end="")
+            print("{}".format(my_list[j]), end="")
             i += 1
         except IndexError:
             break
