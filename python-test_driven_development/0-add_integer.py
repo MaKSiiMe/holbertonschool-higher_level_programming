134560b80446c4e8348b97a4e9750fc2d0e97efe
MaKSiiMe <maxime.truel@gmail.com>
2025-01-24 10:22:37 +0100

    pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
+
 """
 Module that adds two integers
-1
 2
+3
 """
+
+
 def add_integer(a, b=98):
+
     """
     Function that adds two integers
     """
+
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
