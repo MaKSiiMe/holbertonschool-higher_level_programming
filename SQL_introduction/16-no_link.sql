0000cc29e8a7fc4a5ee46e17d65631b81012c8b1
MaKSiiMe <maxime.truel@gmail.com>
2025-03-05 11:27:00 +0100

    add name

--- SQL_introduction/16-no_link.sql
+++ SQL_introduction/16-no_link.sql
@@ -1,3 +1,3 @@
 -- 16. Say my name
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server.
-SELECT score FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
+SELECT score, name FROM second_table WHERE name IS NOT NULL ORDER BY score DESC;
