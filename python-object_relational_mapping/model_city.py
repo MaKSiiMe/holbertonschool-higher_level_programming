5cdcc89a3adb82d807d6141a35e910e057441c5a
MaKSiiMe <maxime.truel@gmail.com>
2025-03-07 17:05:19 +0100

    docstring

--- python-object_relational_mapping/model_city.py
+++ python-object_relational_mapping/model_city.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-
+"""class definition of a City and an instance Base = declarative_base()"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
